1912384b0767b19f78f19fab98e65777ffa1ca5b
LordFenix79 <jcesar.bfx@gmail.com>
2022-06-21 22:04:46 +0000

    Modificaciones hasta capitulo 7 de curso

--- SAPUI5/webapp/controller/App.controller.js
+++ SAPUI5/webapp/controller/App.controller.js
@@ -1,24 +1,18 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/m/MessageToast",
 ], 
 /**
  * 
  * @param {typeof sap.ui.core.mvc.Controller} Controller 
- * @param {typeof sap.m.MessageToast} MessageToast 
- * @param {typeof sap.ui.model.resource.ResourceModel} ResourceModel 
  */
-function(Controller, MessageToast, Models, ResourceModel) {
+function(Controller) {
     "use strict";
 
     return Controller.extend("logaligroup.SAPUI5.controller.App", {
         onInit: function(){
         },
-        onShowHello: function(){
-            var oBundle = this.getView().getModel("i18n").getResourceBundle();
-            var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-            var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-            MessageToast.show(sMsg);
+        onOpenDialogHeader: function(){
+            this.getOwnerComponent().openHelloDialog();
         }
     });
 });
